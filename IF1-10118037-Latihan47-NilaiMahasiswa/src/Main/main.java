a1b859d3864e8deed0cdb274f41732cc4fa2468f
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-10-20 10:33:20 +0700

    Add files via upload

---
+++ IF1-10118037-Latihan47-NilaiMahasiswa/src/Main/main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Muhammad Ihsan
+ * NAMA       : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM          : 10118037
+ * Deskripsi Program   : Program ini berisi program menghitung  nilai mahasiswa
+ *                                        
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NilaiMhs mhs = new NilaiMhs(75,45,34);
+        NilaiMhs mhs1 = new NilaiMhs(60, 80, 75);
+        NilaiMhs mhs2 = new NilaiMhs(30, 80, 40);
+        
+        
+    }
+    
+}
